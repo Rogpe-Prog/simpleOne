8f9484a22209da0a17754806f663db3fc6fe4780
rogpe <prog_x@hotmail.com>
2021-10-01 13:56:47 -0300

    simple get and post array spring

---
+++ src/main/java/dev/heroes/firstAgain/FirstAgainApplication.java
@@ -0,0 +1,13 @@
+package dev.heroes.firstAgain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAgainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAgainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/heroes/firstAgain/FirstAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.heroes.firstAgain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAgainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
